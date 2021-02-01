88cf952f0121d4e3533914a195f4527a92e412f3
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 17:09:13 -0600

    :construction: Use 'split' to turn name into an array of 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // Have to destructure the named export from lib
 import { studentData } from "./lib";
 
-console.log(studentData);
+function getLastName(name) {
+  /**
+   * Assume that there is a space and it's 'first last'
+   * 1. TODO: Turn this into an array ['first', 'last']
+   * 2. Return [1] (index 1)
+   */
+
+  return name.split(" ");
+}
+
+console.log(getLastName("Harry Wolff"));
+
+// console.log(studentData);
 // Get a list of all students last names and ids - only those that scored below 70
 // filter-map chain
 const lowScores = studentData
@@ -11,4 +23,4 @@ const lowScores = studentData
     id: student.id,
   }));
 
-console.log(lowScores);
+// console.log(lowScores);
