fca801552f45c42d2b0967024285073c26fed23c
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 17:11:33 -0600

    :sparkles: Get the student last name in filtered data

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@
 import { studentData } from "./lib";
 
 function getLastName(name) {
-  /**
-   * Assume that there is a space and it's 'first last'
-   * 1. TODO: Turn this into an array ['first', 'last']
-   * 2. Return [1] (index 1)
-   */
-
-  return name.split(" ");
+  return name.split(" ")[1];
 }
 
-console.log(getLastName("Harry Wolff"));
-
-// console.log(studentData);
-// Get a list of all students last names and ids - only those that scored below 70
-// filter-map chain
 const lowScores = studentData
   .filter((student) => student.score < 70)
   .map((student) => ({
-    lastName: student.name,
+    lastName: getLastName(student.name),
     id: student.id,
   }));
 
-// console.log(lowScores);
+console.log(lowScores);
