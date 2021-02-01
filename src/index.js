9191677feb4c45698db2f0f7ff3c6b83b800fcf0
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 16:51:42 -0600

    :construction: Log 'low scores'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// Have to destructure the named export from lib
 import { studentData } from "./lib";
 
 console.log(studentData);
+// Get a list of all students last names and ids - only those that scored below 70
+// filter-map chain
+const lowScores = studentData.filter((student) => student.score < 70);
+
+console.log(lowScores);
