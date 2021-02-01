33c0641e6f074f38fbeb6d92545fd7478d95723f
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 17:22:19 -0600

    :recycle: Destructure 'score' from the student object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { getLastName, studentData } from "./lib";
 
 const lowScores = studentData
-  .filter((student) => student.score < 70)
+  .filter(({ score }) => score < 70)
   .map((student) => ({
     lastName: getLastName(student.name),
     id: student.id,
