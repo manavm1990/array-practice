371a7f6821a3390146d6dd1ffc2a016da2072bf3
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 18:59:58 -0600

    Add example of female senators

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // Have to destructure the named export from lib
 import { getBirthYear, senatorsData } from "./lib";
 
-// TODO: We need to make a list of all female senators to call them and invite to a special event (we need 'phone' and 'name')
+const femaleSenatorNames = senatorsData
+  .filter(({ person }) => person.gender === "female")
+  .map(({ person }) => ({ name: person.name }));
+
+console.log(femaleSenatorNames);
 
 const youngSenators = senatorsData
   .filter(({ person }) => getBirthYear(person.birthday) > 1960)
   .map(({ person }) => ({ name: person.name }));
 
-console.log(youngSenators);
+// console.log(youngSenators);
