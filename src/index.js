db781eaaff4d63d991b7358799359b78ce439de0
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 18:34:34 -0600

    Remove student work

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 // Have to destructure the named export from lib
-import { assignLetterGrade, curveScore, getLastName, studentData } from "./lib";
+import { senatorsData } from "./lib";
 
 // TODO: Get a list of just the 'name' field from the senators if they were born since 1960
-
-const lowScores = studentData
-  .filter(({ score }) => score < 70)
-  .map(({ name, id }) => ({
-    lastName: getLastName(name),
-    id,
-  }));
-
-const scoresWithLetterGrades = studentData.map((student) => ({
-  grade: assignLetterGrade(student.score),
-  ...student,
-}));
-
-const curvedScores = studentData.map((student) => ({
-  curvedScore: curveScore(student.score, 10),
-  ...student,
-}));
-
-// console.log(curvedScores);
+console.log(senatorsData);
