ff36545b500a79eebd326aafaa47debf5255900a
Manav Misra <manavm@visionwebsoft.com>
2021-02-01 18:48:49 -0600

    :construction: Extract all senator names

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { senatorsData } from "./lib";
 // TODO: We need to make a list of all female senators to call them and invite to a special event (we need 'phone' and 'name')
 
 // TODO: Get a list of just the 'name' field from the senators if they were born since 1960
-const youngSenators = senatorsData.map(({ person }) => person);
+const youngSenators = senatorsData.map(({ person }) => ({ name: person.name }));
 
 console.log(youngSenators);
